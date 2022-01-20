c5e124a1073f4c6cac9d589d5b0cb0a5ae0895cb
gabadyj <gabadyj@gmail.com>
2022-01-20 15:27:36 -0300

    finished project

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,26 +4,7 @@ declare let process: {
   env: {
     NODE_ENV: 'dev';
     PORT: number;
-    DB_TYPE: 'postgres';
-    DB_URL?: string;
-    DB_HOST: string;
-    DB_PORT: number;
-    DB_USERNAME: string;
-    DB_PASSWORD: string;
-    DB_NAME: string;
-    DB_LOGS: boolean;
   };
 };
 
-export const {
-  NODE_ENV = 'dev',
-  PORT = 3333,
-  DB_TYPE = 'postgres',
-  DB_URL,
-  DB_HOST = 'localhost',
-  DB_PORT = 5433,
-  DB_USERNAME = 'postgres',
-  DB_PASSWORD = 'docker',
-  DB_NAME = 'hox_api_challenge',
-  DB_LOGS = true,
-} = process.env;
+export const { NODE_ENV = 'dev', PORT = 3333 } = process.env;
